e020230a2b57cb365bc7683df6c5ed70b7d93a05
Step_ri69@itstepru.onmicrosoft.com <Step_ri69@itstepru.onmicrosoft.com>
2022-03-25 22:05:06 +0200

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Template
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int port = 8010;
+        private IPAddress ipServer;
+        private Socket listeningSocket;
+        private EndPoint remotePoint;
+        private Task<string> task;
+        public MainWindow()
+        {
+            InitializeComponent();
+            String host = Dns.GetHostName();
+            ipServer = Dns.GetHostEntry(host).AddressList[4];
+            IPServ.Content = ipServer.ToString();
+           
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string message = Mes.Text;
+                Socket listening = new Socket(AddressFamily.InterNetwork,
+                         SocketType.Dgram, ProtocolType.Udp);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                EndPoint remotePoint = new IPEndPoint(IPAddress.Parse(IPClient.Text), port);
+                listening.SendTo(data, remotePoint);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            await Task.Run(()=>Listen());
+        }
+        private void Listen()
+        {
+                  StringBuilder builder = new StringBuilder();
+                  IPEndPoint ipPoint = new IPEndPoint(ipServer, port);
+                  listeningSocket = new Socket(AddressFamily.InterNetwork,
+                      SocketType.Dgram, ProtocolType.Udp);
+                  listeningSocket.Bind(ipPoint);
+                  try
+                  {
+                      while (true)
+                      {
+
+                          byte[] data = new byte[256];
+                          remotePoint = new IPEndPoint(IPAddress.Any, 0);
+                          int bytes = 0;
+                          do
+                          {
+                              bytes = listeningSocket.ReceiveFrom(data, ref remotePoint);
+                              builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                          }
+                          while (listeningSocket.Available > 0);
+                          IPEndPoint remoteFullIp = remotePoint as IPEndPoint;
+                          Dispatcher.Invoke(()=>Message.Text=builder.ToString());
+                      }
+                  }
+                  catch (Exception ex)
+                  {
+                      MessageBox.Show(ex.Message);
+                  }
+                  finally
+                  {
+                      if (listeningSocket != null)
+                      {
+                          listeningSocket.Shutdown(SocketShutdown.Both);
+                          listeningSocket.Close();
+                          listeningSocket = null;
+                      }
+                  }
+        }
+
+    }
+}
